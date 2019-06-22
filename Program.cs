2f7f9f448fb7b1a71a91ab07de494696a515c6c7
avidme <35063731+avidme@users.noreply.github.com>
2019-06-22 22:15:31 +0530

    Building heap based out of given array
    Build heap based out of given array is yet to be fully implemented in C#

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heapsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void BuildHeap(int[]arr)
+        {
+            int i = 0;
+            int parent = arr[i];
+            int childIndex, parentIndex; 
+            for(i=1;i<arr.Length;i++)
+            {
+                InsertNode(arr[i], parent, arr);
+            }
+
+        }
+
+        static void InsertNode(int node,int parent,int[] arr)
+        {
+            int parentIndex, childIndex; 
+            if(node<=parent)
+            {
+                parentIndex = Array.IndexOf(arr, parent);
+                childIndex = 2 * parentIndex + 1;
+            }
+            else
+            {
+                parentIndex = Array.IndexOf(arr, parent);
+                childIndex = 2 * parentIndex + 2;
+            }
+            if(arr[childIndex]!=0)
+            {
+                InsertNode(node, arr[childIndex], arr);
+            }
+            else
+            {
+                arr[childIndex] = node;
+            }
+        }
+    }
+}
